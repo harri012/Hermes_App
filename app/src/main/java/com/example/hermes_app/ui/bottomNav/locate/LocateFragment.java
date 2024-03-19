8fd872ffc3f5671a7b622c63144d873c49aa6eb7
harri012 <harrisson.tang12@gmail.com>
2024-03-19 19:10:14 -0400

    Get Current Location and Database

--- app/src/main/java/com/example/hermes_app/ui/bottomNav/locate/LocateFragment.java
+++ app/src/main/java/com/example/hermes_app/ui/bottomNav/locate/LocateFragment.java
@@ -4,17 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.hermes_app.R;
 import com.example.hermes_app.databinding.FragmentLocateBinding;
 
 public class LocateFragment extends Fragment {
 
     private FragmentLocateBinding binding;
+    Button locateButton;
+    Button databaseLocationButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +30,27 @@ public class LocateFragment extends Fragment {
 
         final TextView textView = binding.textLocate;
         locateViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        //locate Button Code -> action done by viewmodel
+        locateButton = root.findViewById(R.id.locate_button);
+
+        locateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                locateViewModel.buttonRedirect(requireContext());
+            }
+        });
+
+        //database locate button
+        databaseLocationButton = root.findViewById(R.id.databaseLocation_button);
+
+        databaseLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //code
+                locateViewModel.databaseLocateRedirect(requireContext());
+            }
+        });
         return root;
     }
 
