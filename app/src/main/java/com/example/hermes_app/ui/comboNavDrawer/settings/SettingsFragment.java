0e8f7de313b1a1149d9c2f1e9e127dbba6136420
LuatNguyen18 <85182587+LuatNguyen18@users.noreply.github.com>
2024-03-20 01:10:09 -0400

    Settings prototype Missing Theme change, Remove UUID, etc

--- app/src/main/java/com/example/hermes_app/ui/comboNavDrawer/settings/SettingsFragment.java
+++ app/src/main/java/com/example/hermes_app/ui/comboNavDrawer/settings/SettingsFragment.java
@@ -4,28 +4,41 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.widget.Spinner;
+
+import com.example.hermes_app.R;
 import com.example.hermes_app.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    String[] textArray = { "Normal", "Protanopia", "Tritanopia", "Deuteranopia" };
+    Integer[] imageArray = { R.drawable.normal, R.drawable.protanopia,
+            R.drawable.tritanopia, R.drawable.deuteranopia };
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
         SettingsViewModel settingsViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSettings;
-        settingsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        TextView text = (TextView) root.findViewById(R.id.spinnerTextView);
+        ImageView imageView =(ImageView)root.findViewById(R.id.spinnerImages);
+        Spinner spinner = (Spinner) root.findViewById(R.id.mySpinner);
+
+        SpinnerAdapter adapter = new SpinnerAdapter(root.getContext(), R.layout.spinner_value_layout, textArray, imageArray);
+        spinner.setAdapter(adapter);
+
         return root;
     }
 
--- app/src/main/java/com/example/hermes_app/ui/comboNavDrawer/settings/SettingsViewModel.java
+++ app/src/main/java/com/example/hermes_app/ui/comboNavDrawer/settings/SettingsViewModel.java
@@ -1,19 +1,35 @@
 package com.example.hermes_app.ui.comboNavDrawer.settings;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import java.util.List;
+import java.util.Arrays;
 
 public class SettingsViewModel extends ViewModel {
+    private MutableLiveData<String> textLiveData = new MutableLiveData<>();
+    private MutableLiveData<List<String>> colorPaletteLiveData = new MutableLiveData<>();
+    private MediatorLiveData<String> combinedLiveData = new MediatorLiveData<>();
 
-    private final MutableLiveData<String> mText;
 
     public SettingsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is settings fragment");
+
+
+        textLiveData.setValue("Color Palettes");
+        List<String> colorPaletteNames = Arrays.asList("normal", "protanopia", "deuteranopia", "tritanopia");
+        colorPaletteLiveData.setValue(colorPaletteNames);
+
+
+
     }
 
     public LiveData<String> getText() {
-        return mText;
+        return textLiveData;
     }
+
+    public LiveData<List<String>> getColorPaletteLiveData() {
+        return colorPaletteLiveData;
+    }
+
 }
